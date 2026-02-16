e67ee75bb451b1a01de906e3fe1aaf5a97813553
Fabricio Sousa <ffabricioteixeira15@gmail.com>
2026-02-16 12:44:05 -0300

    Primeiro commit

--- internet.py
+++ internet.py
@@ -1,12 +1,13 @@
 import socket
 import speedtest
 import time
+import emoji
 
-print("Iniciando teste............\n")
+print(emoji.emojize("Iniciando teste:globe_with_meridians:............\n"))
 st = speedtest.Speedtest()
 time.sleep(2)
 
-print("Escolhendo o melhor servidor.......\n")
+print(emoji.emojize("Escolhendo o melhor servidor:satellite_antenna:\n"))
 st.get_best_server()
 time.sleep(2)
 
@@ -35,7 +36,7 @@ print(f"Upload:{upload_mbps:.2f} mbps")
 print("****************************")
 time.sleep(3)
 
-print("\n\nTEstando conexão com a internet............")
+print(emoji.emojize("\n\nTestando conexão com a internet:globe_with_meridians:............"))
 
 print("Criar socket......")
 sc = socket.socket()
@@ -53,7 +54,7 @@ ping_socket = (tempo_final -tempo_inicial ) * 1000
 print("\nconexão bem sucedida*****")
 print(f"Ping (socket): {ping_socket:.2f} ms")
 
-print("Fechar********")
+print("*************Fechar********")
 sc.close()
 
 
