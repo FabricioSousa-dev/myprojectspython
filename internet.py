4c9647c8ed568abdf79060449c1bda5e15ee183f
Fabricio Sousa <ffabricioteixeira15@gmail.com>
2026-02-15 23:18:34 -0300

    atualização

--- internet.py
+++ internet.py
@@ -1,3 +1,4 @@
+import socket
 import speedtest
 import time
 
@@ -23,12 +24,36 @@ time.sleep(2)
 
 
 dowmload_mbps = download / 1_000_000
-opload_mbps = upload / 1_000_000
+upload_mbps = upload / 1_000_000
+time.sleep(5)
 
 print("\n\n\n*******************Resultado do teste*************************\n\n")
 print(f"Ping:{ping:.2f}ms")
-print(f"Download:{dowmload_mbps:.2f}mps")
-print(f"Upload:{upload:.2f}mbps")
+print(f"Download:{dowmload_mbps:.2f} mps")
+print(f"Upload:{upload_mbps:.2f} mbps")
 
 print("****************************")
+time.sleep(3)
+
+print("\n\nTEstando conexão com a internet............")
+
+print("Criar socket......")
+sc = socket.socket()
+
+print("\nMedir tempo.......")
+tempo_inicial = time.time()
+
+print("\nConectar.......")
+connection = sc.connect(('www.google.com', 80))
+
+
+tempo_final = time.time()
+ping_socket = (tempo_final -tempo_inicial ) * 1000
+
+print("\nconexão bem sucedida*****")
+print(f"Ping (socket): {ping_socket:.2f} ms")
+
+print("Fechar********")
+sc.close()
+
 
