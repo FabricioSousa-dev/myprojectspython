4ecef4f587419f7f471c549697095c28b635e720
FabricioSousa-dev <ffabricioteixeira15@gmail.com>
2026-02-17 14:45:17 -0300

    Atualizção

--- internet.py
+++ internet.py
@@ -4,39 +4,39 @@ import time
 import emoji
 
 print(emoji.emojize("Iniciando teste:globe_with_meridians:............\n"))
+time.sleep(4)
 st = speedtest.Speedtest()
-time.sleep(2)
+
 
 print(emoji.emojize("Escolhendo o melhor servidor:satellite_antenna:\n"))
 st.get_best_server()
-time.sleep(2)
+
 
 print("Testando o ping.......\n")
 ping = st.results.ping
-time.sleep(2)
+
 
 print("Testar download.......\n")
 download = st.download()
-time.sleep(2)
+
 
 print("Testar upload.......\n")
 upload = st.upload()
-time.sleep(2)
+
 
 
 download_mbps = download / 1_000_000
 upload_mbps = upload / 1_000_000
-time.sleep(5)
 
-print("\n\n\n*******************Resultado do teste*************************\n\n")
+print("\n\n*******************Resultado do teste*************************\n")
 print(f"Ping:{ping:.2f}ms")
 print(f"Download:{download_mbps:.2f} mbps")
 print(f"Upload:{upload_mbps:.2f} mbps")
 
 print("****************************")
-time.sleep(3)
+time.sleep(5)
 
-print(emoji.emojize("\n\nTestando conexão com a internet:globe_with_meridians:............"))
+print(emoji.emojize("\n\n************Testando conexão com a internet:globe_with_meridians:............"))
 
 print("Criar socket......")
 sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,7 +54,8 @@ ping_socket = (tempo_final -tempo_inicial ) * 1000
 print("\nconexão bem sucedida*****")
 print(f"Ping (socket): {ping_socket:.2f} ms")
 
-print("*************Fechar********")
+print("\n\n******Fechando conexão********")
 sc.close()
 
+print("Funcionou até aqui,obrigado pela paciência!")
 
